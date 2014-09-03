f3a7bb08b21fe78f32c32674631080f1cb6d4e1c
archangelmichael <gufy_el_passo@yahoo.com>
2014-09-04 01:53:21 +0300

    Final Version with working SQLite MySQL connections
    Corrected path to SQLite and a lot of other changes to make it work.
    Created a report based on MySQL and SQLite data.

--- NinjaFactory/NinjaFactory.ExcelReporting/ExcelSuccessRateReportCreator.cs
+++ NinjaFactory/NinjaFactory.ExcelReporting/ExcelSuccessRateReportCreator.cs
@@ -16,50 +16,63 @@
     /// </summary>
     public class ExcelSuccessRateReportCreator
     {
-        public const string SQLITE_DATABASE = @"../../../ninja_specialties.sqlite";
+        //private const string SQLitePath = "Data Source=../../../ninja_specialties.db;Version=3;New=False;Compress=True;";
 
-        public Dictionary<string, int> LoadSQliteData(string databasePath)
+        public Dictionary<string, string> LoadSQliteData()
         {
-            string sqlQuery = "SELECT Specialty, Weight FROM specialties;";
-            var specialties = new Dictionary<string, int>();
-            var connection = new SQLiteConnection("Data Source=" + databasePath + ";Version=3;");
-
-            connection.Open();
-
-            var command = new SQLiteCommand(sqlQuery, connection);
-
-            SQLiteDataReader reader = command.ExecuteReader();
-
-            while (reader.Read())
+            var specialties = new Dictionary<string, string>();
+            try
             {
-                specialties.Add((string)reader[0], (int)reader[1]);
-            }
+                var connection = new SQLiteConnection("Data Source=ninja_specialties.db;Version=3;New=False;Compress=True;");
+                connection.Open();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT Specialty, Weight FROM Specialties";
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    string specialty = reader[0].ToString();
+                    string weight = reader[1].ToString();
+                    specialties.Add(specialty, weight);
+                }
 
+                connection.Close();
+            }
+            catch (Exception exc)
+            {
+                
+                throw new ArgumentException(exc.Message);
+            }
+            
             return specialties;
         }
 
         public void CreateSuccessRateReport(INinjaCatalogueModelUnitOfWork db, string filePath)
         {
-            IEnumerable<SuccessRateReport> successRateReport;
-
-            //var specialties = this.LoadSQliteData(SQLITE_DATABASE);
-
-            successRateReport = this.SelectSuccessRateList(db);
-            this.WriteToFile(successRateReport, filePath);
+            IEnumerable<NinjaReport> successRateReport;
+            var specialties = LoadSQliteData();
+            successRateReport = SelectSuccessRateList(db, specialties);
+            WriteToFile(successRateReport, filePath); 
         }
 
-        private IEnumerable<SuccessRateReport> SelectSuccessRateList(INinjaCatalogueModelUnitOfWork db)
+        private IEnumerable<NinjaReport> SelectSuccessRateList(INinjaCatalogueModelUnitOfWork db, Dictionary<string, string> specialties)
         {
-            return db.Ninja_catalogue_items
+            var ninjas = db.Ninja_catalogue_items
                 .OrderBy(item => item.SuccessRate)
-                .Select(item => new SuccessRateReport()
+                .Select(item => new NinjaReport()
                        {
-                           NinjaCatalogueItem = item,
-                       })
-                .ToList<SuccessRateReport>();
+                           Id = item.Id,
+                           Name = item.Name,
+                           Weapon = item.Weapon,
+                           SpecialtyWeight = int.Parse(specialties[item.Speciality]),
+                           Speciality = item.Speciality,
+                           SuccessfulJobsCount = item.SuccessfulJobsCount,
+                           KillCount = item.KillCount,
+                           SuccessRate = item.SuccessRate
+                       }).ToList<NinjaReport>();
+            return ninjas.OrderByDescending(ninja => ninja.SpecialtyWeight);
         }
 
-        private void WriteToFile(IEnumerable<SuccessRateReport> successRateReportList, string filePath)
+        private void WriteToFile(IEnumerable<NinjaReport> successRateReportList, string filePath)
         {
             // Create the file using the FileInfo object
             var excelFile = File.Create(filePath);
@@ -78,23 +91,23 @@
                 worksheet.Cells[1, 1].Value = "Id";
                 worksheet.Cells[1, 2].Value = "Name of the ninja";
                 worksheet.Cells[1, 3].Value = "Weapon used";
-                worksheet.Cells[1, 4].Value = "Speciality";
-                worksheet.Cells[1, 5].Value = "Jobs Count";
-                worksheet.Cells[1, 6].Value = "Jobs Count";
+                worksheet.Cells[1, 4].Value = "Speciality weight";
+                worksheet.Cells[1, 5].Value = "Speciality";
+                worksheet.Cells[1, 6].Value = "Jobs Done";
                 worksheet.Cells[1, 7].Value = "Kill Count";
                 worksheet.Cells[1, 8].Value = "Success Rate";
 
                 var rowIndex = 2;
                 foreach (var report in successRateReportList)
                 {
-                    worksheet.Cells[rowIndex, 1].Value = report.NinjaCatalogueItem.Id;
-                    worksheet.Cells[rowIndex, 2].Value = report.NinjaCatalogueItem.Name;
-                    worksheet.Cells[rowIndex, 3].Value = report.NinjaCatalogueItem.Weapon;
-                    worksheet.Cells[rowIndex, 4].Value = report.NinjaCatalogueItem.Speciality;
-                    worksheet.Cells[rowIndex, 5].Value = report.NinjaCatalogueItem.JobsCount;
-                    worksheet.Cells[rowIndex, 6].Value = report.NinjaCatalogueItem.SuccessfulJobsCount;
-                    worksheet.Cells[rowIndex, 7].Value = report.NinjaCatalogueItem.KillCount;
-                    worksheet.Cells[rowIndex, 8].Value = report.NinjaCatalogueItem.SuccessRate;
+                    worksheet.Cells[rowIndex, 1].Value = report.Id;
+                    worksheet.Cells[rowIndex, 2].Value = report.Name;
+                    worksheet.Cells[rowIndex, 3].Value = report.Weapon;
+                    worksheet.Cells[rowIndex, 4].Value = report.SpecialtyWeight;
+                    worksheet.Cells[rowIndex, 5].Value = report.Speciality;
+                    worksheet.Cells[rowIndex, 6].Value = report.SuccessfulJobsCount;
+                    worksheet.Cells[rowIndex, 7].Value = report.KillCount;
+                    worksheet.Cells[rowIndex, 8].Value = report.SuccessRate;
 
                     rowIndex++;
                 }
--- NinjaFactory/NinjaFactory.ExcelReporting/SuccessRateReport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NinjaFactory.ExcelReporting
-{
-    using NinjaFactory.DataBase.MySql;
-
-    public class SuccessRateReport
-    {
-        public Ninja_catalogue_item NinjaCatalogueItem { get; set; }
-    }
-}
