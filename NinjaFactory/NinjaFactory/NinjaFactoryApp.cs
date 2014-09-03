cc13a68a08a5134b4b8237b35fc7985ed402048b
etcet1 <e_grekov@abv.bg>
2014-09-03 20:29:04 +0300

    EPP library added

--- NinjaFactory/NinjaFactory/NinjaFactoryApp.cs
+++ NinjaFactory/NinjaFactory/NinjaFactoryApp.cs
@@ -134,7 +134,7 @@
 
                 ExcelSuccessRateReportCreator excelCreator = new ExcelSuccessRateReportCreator();
                 excelCreator.CreateSuccessRateReport(mySqlDb, filePath);
-                MessageBox.Show("NinjaRankingReport report generated!");
+                MessageBox.Show("NinjaRankingReport generated!");
             }
         }
 
